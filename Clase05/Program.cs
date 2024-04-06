bc649754d412c11832b364bf41eb7970e94fcde9
User <User@DESKTOP-J0SKJCN>
2024-04-05 22:00:39 -0500

    Agregar archivos de proyecto.

---
+++ Clase05/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection.Metadata.Ecma335;
+
+Console.WriteLine("Hello, World!");
+
+
+int cantidad = 0;
+double promedioArreglo = 0;
+
+Console.WriteLine("INGRESW CANRIDAD  NOTAS");
+cantidad = Convert.ToInt32(Console.ReadLine());
+
+int[] notas = new int[cantidad];
+
+for (int i = 0; i < cantidad; i++)
+{
+    int numeroNota = i + 1;
+    Console.WriteLine("Ingrese nota ["+ numeroNota + "]");
+    notas[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+
+double calcularPromedio(int[] notas)
+{
+    int sumaNotas = 0;
+   
+    int max = notas[0], min = notas[0];
+
+    // editificr menor y mayor
+    for (int i = 0; i < cantidad; i++)
+    {
+        if (notas[i] < min)
+        {
+            min = notas[i];
+        }
+        else if (notas[i] > max)
+        {
+            min = notas[i];
+        }
+
+        sumaNotas = sumaNotas + notas[i];
+    }
+
+    promedioArreglo = (sumaNotas - max - min) / (cantidad - 2);
+
+    return promedioArreglo;
+}
+
+
+promedioArreglo = calcularPromedio(notas);
+
+Console.WriteLine(promedioArreglo);
+
+
+
+
---
+++ listas/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// VAMOS A VER  LISTAS.
+List<string> asistentes = new List<string>();
+
+string nombre = string.Empty;
+bool salir = false;
+string letra = string.Empty;
+
+do
+{
+    Console.WriteLine("Ingrese nombre del asistente");
+    nombre = Console.ReadLine();
+    asistentes.Add(nombre);
+
+    Console.WriteLine("Presiona la S para salir");
+    letra = Console.ReadLine();
+
+    if (letra.ToUpper() == "S")
+        salir = true;
+} while (!salir);
+
+Console.WriteLine("La lista  de Asistentes es:");
+foreach(var item in asistentes)
+{
+    Console.WriteLine(item);
+}
+
